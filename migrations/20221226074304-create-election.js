fdfe926290aa834ed82626c21314963b90730786
Akshithagulle <113706869+Akshithagulle@users.noreply.github.com>
2023-01-19 21:37:07 +0530

    Add files via upload

---
+++ migrations/20221226074304-create-election.js
@@ -0,0 +1,35 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Elections", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      state: {
+        type: Sequelize.STRING,
+        defaultValue: "new",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Elections");
+  },
+};
